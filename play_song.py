e91c8ef04837bb8e0616eb07f21d372f7a08b69b
dileepsankhla <sankhla.dileep96@gmail.com>
2016-10-31 12:29:43 +0530

    added incomplete play_song.py

---
+++ play_song.py
@@ -0,0 +1,20 @@
+import os,time
+from random import randint
+
+def play_song():
+    sum = 0
+    count = 0
+    txt = open(".songslist.txt", "r")
+    song_list = txt.read().splitlines()
+    for i,val in enumerate(song_list):
+        if val.isdigit():
+            sum = sum + int(val)
+            count = count + 1
+    average = sum / count
+    #average is calculated
+
+
+
+
+if __name__ == "__main__":
+    play_song()
